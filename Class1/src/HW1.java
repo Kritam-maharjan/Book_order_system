bb79167aee214942ff5dbc87dfe358b45b7bc649
Manjit Maharjan <maharjanmanjit46@gmail.com>
2024-08-05 09:15:14 +0545

    inihi

---
+++ Class1/src/HW1.java
@@ -0,0 +1,20 @@
+public class HW1 {
+    public static void main(String[] args) {
+
+        int num1 = 34;
+        int num2 = 55;
+        int sum = num1+num2;
+        System.out.println(sum);
+
+        int prin = 100000;
+        int rate = 8;
+        int time = 3;
+        int SI = (prin * rate*time) / 100;
+        System.out.println(SI);
+
+        int lenth = 10;
+        int breadth = 12;
+        int area = lenth*breadth;
+        System.out.println("The area of the rectangle is "+area);
+    }
+}
